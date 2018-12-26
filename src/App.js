b03cfe96de5185f844da0c8514658cd3a0eceaf7
Andrew Savala <andrew@redswimmer.com>
2018-12-26 10:57:04 -0800

    added routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
+
 import "./App.css";
 
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-        <Movies />
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Route path="/" exact component={Movies} />
+          <Redirect to="/not-found" />
+        </Switch>
       </main>
     );
   }
---
+++ src/components/rentals.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Rentals extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h1>Rentals</h1>;
+  }
+}
+
+export default Rentals;
