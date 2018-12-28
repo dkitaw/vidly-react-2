61662fa40aafca9dedb264357b20d47cbf14f56e
Andrew Savala <andrew@redswimmer.com>
2018-12-27 16:48:55 -0800

    added button and route for adding a new movie

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -90,6 +90,14 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
+          <p>
+            <button
+              className="btn btn-primary"
+              onClick={() => this.props.history.push("/movies/new")}
+            >
+              New Movie
+            </button>
+          </p>
           <p>Showing {movieCount} movies in the database.</p>
           <MoviesTable
             movies={movies}
