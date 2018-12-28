5b2921118a5572eefb7233ba68a7bc8f7fe9774e
Andrew Savala <andrew@redswimmer.com>
2018-12-28 14:39:50 -0800

    refactored New Movie button to be a Link

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { getMovies } from "../services/fakeMovieService";
 import { getGenres } from "../services/fakeGenreService";
 import MoviesTable from "./moviesTable";
@@ -90,14 +91,13 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <p>
-            <button
-              className="btn btn-primary"
-              onClick={() => this.props.history.push("/movies/new")}
-            >
-              New Movie
-            </button>
-          </p>
+          <Link
+            to="/movies/new"
+            className="btn btn-primary"
+            style={{ marginBottom: 20 }}
+          >
+            New Movie
+          </Link>
           <p>Showing {movieCount} movies in the database.</p>
           <MoviesTable
             movies={movies}
