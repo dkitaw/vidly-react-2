8e0c73e218187b3b0bde6cb83209e645b3741c6d
Andrew Savala <andrew@redswimmer.com>
2019-01-02 17:26:07 -0800

    dynamically update the menu when user logs in/out

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends Component {
     return (
       <>
         <ToastContainer />
-        <NavBar />
+        <NavBar user={this.state.user} />
         <main className="container">
           <Switch>
             <Route path="/login" component={LoginForm} />
