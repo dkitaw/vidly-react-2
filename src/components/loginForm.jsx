b430f90eff88227169e2d8c3e67af6aeade7da67
Andrew Savala <andrew@redswimmer.com>
2019-01-02 16:40:46 -0800

    login user on login page and store jwt

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -21,7 +21,9 @@ class LoginForm extends Form {
   doSubmit = async () => {
     try {
       const { username, password } = this.state.data;
-      await login(username, password);
+      const { data: jwt } = await login(username, password);
+      localStorage.setItem("token", jwt);
+      this.props.history.replace("/");
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
         const errors = { ...this.state.errors };
